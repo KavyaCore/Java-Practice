93e779056cdcb1e658f48b36f6c1f639263810ac
Kavya Ladha <ladhakavya@gmail.com>
2026-08-01 19:28:03 +0530

    Add Leap Year checker program

---
+++ Basics/LeapYear.java
@@ -0,0 +1,20 @@
+// Program to check whether a given year is a leap year.
+
+package basics;
+
+import java.util.Scanner;
+
+public class LeapYear {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a year: ");
+        int year = sc.nextInt();
+
+        System.out.println((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)
+                ? "Leap Year"
+                : "Not a Leap Year");
+    }
+}
