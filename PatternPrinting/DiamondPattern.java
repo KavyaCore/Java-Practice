6a7cd6f416c4120fe633209e7d045614985b9521
Kavya Ladha <ladhakavya@gmail.com>
2026-08-02 23:13:25 +0530

    Add DiamondPattern.java to print diamond star pattern
    This program prompts the user for the number of rows and prints a diamond star pattern accordingly.

---
+++ PatternPrinting/DiamondPattern.java
@@ -0,0 +1,40 @@
+// Program to print a diamond star pattern.
+
+package patternprinting;
+
+import java.util.Scanner;
+
+public class DiamondPattern {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of rows: ");
+        int n = sc.nextInt();
+
+        // Upper half
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j <= n - i; j++)
+                System.out.print(" ");
+
+            for (int j = 1; j <= (2 * i - 1); j++)
+                System.out.print("*");
+
+            System.out.println();
+        }
+
+        // Lower half
+        for (int i = n - 1; i >= 1; i--) {
+
+            for (int j = 1; j <= n - i; j++)
+                System.out.print(" ");
+
+            for (int j = 1; j <= (2 * i - 1); j++)
+                System.out.print("*");
+
+            System.out.println();
+        }
+    }
+}
