474f7dae5d79fddfc33990ae7f374d1959624842
Kavya Ladha <ladhakavya@gmail.com>
2026-08-02 23:14:58 +0530

    Add ButterflyPattern.java to print star pattern
    This program prints a butterfly star pattern based on the number of rows input by the user.

---
+++ PatternPrinting/ButterflyPattern.java
@@ -0,0 +1,46 @@
+// Program to print a butterfly star pattern.
+
+package patternprinting;
+
+import java.util.Scanner;
+
+public class ButterflyPattern {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of rows: ");
+        int n = sc.nextInt();
+
+        // Upper Half
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j <= i; j++)
+                System.out.print("*");
+
+            for (int j = 1; j <= 2 * (n - i); j++)
+                System.out.print(" ");
+
+            for (int j = 1; j <= i; j++)
+                System.out.print("*");
+
+            System.out.println();
+        }
+
+        // Lower Half
+        for (int i = n; i >= 1; i--) {
+
+            for (int j = 1; j <= i; j++)
+                System.out.print("*");
+
+            for (int j = 1; j <= 2 * (n - i); j++)
+                System.out.print(" ");
+
+            for (int j = 1; j <= i; j++)
+                System.out.print("*");
+
+            System.out.println();
+        }
+    }
+}
