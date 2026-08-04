cd80345d3aee38438e470a7fb952eed5423112e7
Kavya Ladha <ladhakavya@gmail.com>
2026-08-04 11:10:54 +0530

    Add Palindrome Number Pyramid program
    This program takes an integer input for the number of rows and prints a pyramid of palindrome numbers.

---
+++ PatternPrinting/PalindromeNumberPyramid.java
@@ -0,0 +1,36 @@
+// Program to print a palindrome number pyramid.
+
+package patternprinting;
+
+import java.util.Scanner;
+
+public class PalindromeNumberPyramid {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of rows: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+
+            // Print spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print("  ");
+            }
+
+            // Descending numbers
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j + " ");
+            }
+
+            // Ascending numbers
+            for (int j = 2; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+
+            System.out.println();
+        }
+    }
+}
